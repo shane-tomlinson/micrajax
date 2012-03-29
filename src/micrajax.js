3f94a28d47887daaf63142b28240c2782e43f6d7
Shane Tomlinson <stomlinson@mozilla.com>
2012-03-29 16:03:51 +0100

    Replace function.prototype.bind with curry

--- src/micrajax.js
+++ src/micrajax.js
@@ -4,28 +4,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 window.Micrajax = (function() {
   "use strict";
-  if (!Function.prototype.bind) {
-    Function.prototype.bind = function (oThis) {
-      if (typeof this !== "function") {
-        // closest thing possible to the ECMAScript 5 internal IsCallable function
-        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-      }
 
-      var aArgs = Array.prototype.slice.call(arguments, 1),
-          fToBind = this,
-          fNOP = function () {},
-          fBound = function () {
-            return fToBind.apply(this instanceof fNOP
-                                   ? this
-                                   : oThis || window,
-                                 aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
+  function curry(fToBind) {
+    var aArgs = [].slice.call(arguments, 1),
+        fBound = function () {
+          return fToBind.apply(null, aArgs.concat([].slice.call(arguments)));
+        };
 
-      fNOP.prototype = this.prototype;
-      fBound.prototype = new fNOP();
-
-      return fBound;
-    };
+    return fBound;
   }
 
   function getXHRObject() {
@@ -107,7 +93,7 @@ window.Micrajax = (function() {
     var xhrObject = getXHRObject();
 
     if (xhrObject) {
-      xhrObject.onreadystatechange = onReadyStateChange.bind(null, xhrObject, callback);
+      xhrObject.onreadystatechange = curry(onReadyStateChange, xhrObject, callback);
 
       var type = (options.type || "GET").toUpperCase(),
           url = getURL(options.url, type, options.data),
